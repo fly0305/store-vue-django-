9489aa0e014e9bd465f7328c58835d699f31ec99
flyasher <flyhighasher@gmail.com>
2021-11-22 20:43:35 -0400

    decimal field bug

--- mystore/store/migrations/0001_initial.py
+++ mystore/store/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('slug', models.SlugField(max_length=20, unique=True)),
                 ('image', models.URLField()),
-                ('price', models.DecimalField(decimal_places=5, max_digits=3)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=5)),
                 ('name', models.CharField(max_length=50)),
                 ('qty', models.PositiveSmallIntegerField()),
                 ('inventory', models.PositiveSmallIntegerField()),
