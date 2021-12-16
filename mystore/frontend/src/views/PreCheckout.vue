d82e08647196ea52ea2dbe3110bb401df525d7cf
flyasher <flyhighasher@gmail.com>
2021-12-16 14:40:25 -0400

    UI change

--- mystore/frontend/src/views/PreCheckout.vue
+++ mystore/frontend/src/views/PreCheckout.vue
@@ -1,64 +1,73 @@
 <template>
   <v-container class="grey lighten-5">
-    <v-row class="justify-center" no-gutters>
-      <v-card v-if="isLogin == false"
-        elevation="1"
-        width="600px"
-        class="mx-auto my-16"
+    <v-row class="justify-center mt-5" no-gutters>
+      <v-col
+        class="pa-3"
+        cols="12"
+        sm="6"
       >
-        <v-row no-gutters>
-          <v-col cols="6">
-            <v-card
-              color="#385F73"
-              class="ma-3"
-              dark
-            >
-              <v-card-title class="text-h5">
-                Shop as a Guest!
-              </v-card-title>
+        <v-card v-if="isLogin == false"
+          elevation="2"
+          class="mx-auto py-3"
+        >
+          <v-card
+            color="#385F73"
+            class="ma-5"
+            dark
+          >
+            <v-card-title class="text-h5">
+              Shop as a Guest!
+            </v-card-title>
 
-              <v-card-subtitle>Fill out a complete form to finish the transaction.</v-card-subtitle>
+            <v-card-subtitle>Fill out a complete form to finish the transaction.</v-card-subtitle>
 
-              <v-card-actions>
-                <v-btn
-                  to='/guest-form'
-                  text
-                >
-                  Shop Now
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-          <v-divider vertical></v-divider>
-          <v-col cols="6">
-            <v-card
-              color="#5A6978"
-              class="ma-3"
-              dark
-            >
-              <v-card-title class="text-h5">
-                Sign In
-              </v-card-title>
+            <v-card-actions>
+              <v-btn
+                to='/guest-form'
+                text
+              >
+                Shop Now
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-card>
+      </v-col>
+      <!-- card separation -->
+      <v-col
+        class="pa-3"
+        cols="12"
+        sm="6"
+      >
+        <v-card v-if="isLogin == false"
+          elevation="2"
+          class="mx-auto py-3"
+        >
+          <v-card
+            color="#5A6978"
+            class="ma-5"
+            dark
+          >
+            <v-card-title class="text-h5">
+              Sign In
+            </v-card-title>
 
-              <v-card-subtitle>Shop with your information in our records.</v-card-subtitle>
+            <v-card-subtitle>Shop with your information in our records.</v-card-subtitle>
 
-              <v-card-actions>
-                <v-btn to='/login' text>
-                  Log In
-                </v-btn>
-                <v-btn
-                  class="hidden-xs-only"
-                  to='/register'
-                  text
-                >
-                  Register
-                </v-btn>
-              </v-card-actions>
-            </v-card>
-          </v-col>
-        </v-row>
-      </v-card>
-
+            <v-card-actions>
+              <v-btn to='/login' text>
+                Log In
+              </v-btn>
+              <v-btn
+                to='/register'
+                text
+              >
+                Register
+              </v-btn>
+            </v-card-actions>
+          </v-card>
+        </v-card>
+      </v-col>
+      <!-- end -->
       <v-col v-if="isLogin == true"
         cols="12"
       >
